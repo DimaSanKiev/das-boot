0f9735f74932bd95099816d731352b6ec9dcd3e8
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-09-24 13:50:22 +0300

    Add ShipwreckControllerTest using mocking objects

---
+++ src/test/java/com/boot/controller/ShipwreckControllerTest.java
@@ -0,0 +1,39 @@
+package com.boot.controller;
+
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class ShipwreckControllerTest {
+
+    @InjectMocks
+    private ShipwreckController sc;
+
+    @Mock
+    private ShipwreckRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testShipwreckGet() throws Exception {
+        Shipwreck sw = new Shipwreck();
+        sw.setId(1L);
+
+        when(repository.findOne(1L)).thenReturn(sw);
+        Shipwreck wreck = sc.get(1L);
+        verify(repository).findOne(1L);
+
+        assertEquals(1L, wreck.getId().longValue());
+    }
+
+}
